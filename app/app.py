99f2a37722077d52dd215cab6d7aae8fb8b1d1ed
Marco Frassineti <m.frassineti@reply.it>
2023-10-26 13:26:28 +0200

    Improve error handling

--- app/app.py
+++ app/app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
-from controllers.user_controller import *
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({"error": "Internal Server Error"}), 500
+
 
 if __name__ == '__main__':
+    from controllers.user_controller import *
     app.run()
--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -2,27 +2,37 @@ from flask import jsonify, request
 
 from app import app
 from services import user_service
+from models.user import User
 
-@app.route('/api/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
+
+@app.get('/api/users/<int:user_id>')
+def get_user(user_id: int):
     user = user_service.get_user(user_id)
     if user is not None:
         return jsonify(user)
     else:
         return jsonify({'error': 'User not found'}), 404
 
-@app.route('/api/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    if data:
-        user = user_service.create_user(data)
-        return jsonify(user), 201
-    else:
-        return jsonify({'error': 'Invalid data'}), 400
 
-@app.route('/api/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
+@app.post('/api/users')
+def create_user():
+    body = request.get_json(silent=True)
+    if body:
+        try:
+            user = User(**body)
+        except TypeError as err:
+            print(type(err))
+            return jsonify({"error": "Invalid request"}), 400
+    else:
+        return jsonify({"error": "Unsupported media type"}), 415
+
+    user = user_service.create_user(user)
+    return jsonify(user), 201
+
+
+@app.delete('/api/users/<int:user_id>')
+def delete_user(user_id: int):
     if user_service.delete_user(user_id):
-        return jsonify({'message': 'User deleted'}), 204
+        return None, 204
     else:
         return jsonify({'error': 'User not found'}), 404
